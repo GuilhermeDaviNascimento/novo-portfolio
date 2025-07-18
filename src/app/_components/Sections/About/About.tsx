48db95308197883fa66a8587c544f18edf2b5b3e
unknown <guiguidavi9@gmail.com>
2025-07-18 15:01:34 -0300

    feat: comecando tela de about-me

---
+++ src/app/_components/Sections/About/About.tsx
@@ -0,0 +1,28 @@
+export default function About() {
+  return (
+    <div className="min-w-screen min-h-screen flex flex-col">
+      <div className=" flex items-center px-48 justify-between w-full h-full gap-5">
+        <div className="relative w-[100%] h-[50%]">
+          <div className="w-[100%] h-[100%] bg-black rounded-md"></div>
+          <div className="p-3 gap-5 absolute bottom-0 right-0 translate-y-1/2 bg-[#0f3d3e] rounded-md justify-center items-center flex shadow-lg">
+            <p className="text-white font-bold">1+ Years experience</p>
+          </div>
+        </div>
+        <div className="flex gap-5 flex-col">
+          <span className="border-[#0f3d3e] border-b-4 text-4xl font-bold w-max pb-5">
+            About me
+          </span>
+
+          <p>
+            I'm a passionate Product Designer and Front-End Developer with a keen eye for creating
+            beautiful, functional, and user-centered digital experiences. My approach combines
+            creative design thinking with technical expertise to build products that not only look
+            great but also solve real problems. Specializing in UI/UX design, React development, and
+            interactive animations, I help brands and startups transform their ideas into polished
+            digital products that users love to engage with.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,11 @@
-import Button from '@/components/Button/Button';
-import { IoIosArrowDown } from 'react-icons/io';
-import { IoIosArrowForward } from "react-icons/io";
-import { FaDownload } from 'react-icons/fa';
+import About from "./_components/Sections/About/About";
+import Home from "./_components/Sections/Home/Home";
 
-export default function Home() {
+export default function Main() {
   return (
-    <div className="bg-[#f1f1f1] w-full h-full flex flex-col">
-      <div className=" flex items-center px-48 justify-between w-full h-full">
-        <div className="flex flex-col gap-2">
-          <span className="text-2xl">Hi, I'm</span>
-          <span className="text-8xl font-bold">Guilherme Davi</span>
-          <span className="text-4xl font-bold border-b-8 border-[#e2dcc8] w-max">
-            Front-end Developer
-          </span>
-          <p>
-            Creating seamless digital experiences with a focus <br /> on user-centered design and
-            clean, efficient code.
-          </p>
-
-          <div className="flex gap-5">
-            <div>
-              <Button>View Projects <IoIosArrowForward /></Button>
-            </div>
-            <div>
-              <Button variation="secondary">
-                Download CV <FaDownload />
-              </Button>
-            </div>
-          </div>
-        </div>
-
-        <div className="relative w-80 h-80">
-          <div className="bg-black w-80 h-80 rounded-full border-white border-8"></div>
-          <div className="p-3 gap-5 absolute bottom-0 right-0 translate-y-1/2 bg-white rounded-xl justify-center items-center flex shadow-lg">
-            <div className="w-3 h-3 bg-green-500 rounded-full"></div>
-            Available for work
-          </div>
-        </div>
-      </div>
-      <div className="flex items-end justify-center">
-        <IoIosArrowDown className="text-3xl animate-bounce text-[#111827]" />
-      </div>
+    <div className="w-full h-full flex flex-col overflow-x-hidden">
+      <Home></Home>
+      <About></About>
     </div>
   );
 }
